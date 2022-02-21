2330b77ae397ac18f8d425bcaaed001e9edb5eaf
xuchen-imac <chenxu2656@gmail.com>
2022-02-21 20:12:37 +0800

    update

--- routes/admin/createuser.js
+++ routes/admin/createuser.js
@@ -1,4 +1,6 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const joi = require('joi')
 //用户集合构造函数
 const {User,createUser} = require('../../model/user');
 module.exports = async function(req, res, next) {
@@ -6,6 +8,20 @@ module.exports = async function(req, res, next) {
     let userInput = req.body
     let {username,email,password,role,state} = userInput
     // res.render('admin/err.art',{err: req.body})
+    // 创建用户数据的验证规则
+    let userSchema = {
+        username: Joi.string().min(6).max(16).required().error(new Error('用户名不符合规范')),
+        email: Joi.string().email().required().error(new Error('邮箱不符合规范')),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).required().error(new Error('密码不符合规范')),
+        // role: Joi.string().valid('normal','admin') //必须是这两个之一
+        role: Joi.string().required().error(new Error('角色非法')),
+        role: Joi.required().error(new Error('状态非法')),
+    }
+    //验证
+    const joiSchema = Joi.object(userSchema)
+    const value = await joiSchema.validateAsync(userInput)
+    console.log(`value: ${value}`);
+
     if (email.trim().length == 0 || password.trim().length == 0 || username.trim().length == 0) {
         return res.status(400).render('admin/err.art',{err: "cuole"})
     }
